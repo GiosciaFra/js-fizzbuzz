232278fb66ad5eb9280cf29c0106c4ec253cf48b
giosciafra <francescogioscia8@gmail.com>
2024-01-31 16:37:49 +0100

    Bonus 1  completato

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
+// manipoliamo il DOM
+const container = document.querySelector('.output-container');
+
+
+
 // inizio ciclo
 
 for (let i = 0; i <= 100; i++) {
+
+
+    // creazione nuovo elemento 'div'
+    const newElement = document.createElement('div');
+    
     let output = '' ;
+
     
     // numeri divisibili per 3
- 
+    
     if (i % 3 == 0) {
         output += 'Fizz'
     } 
-
+    
     // numeri divisibili per 5
-
+    
     if (i % 5 == 0) {
         output += 'Buzz'
     }
-
-    // numeri divisibili sia per 3 che per 5
-
-    // if (i % 3 == 0 && i % 5 == 0) {
-    //     output += 'FizzBuzz'
-    // }
-
+    
     // numeri non divisibili nè per 3 nè per 5
-
+    
     if (output === '') {
         output = i;
     }
-
+    
+    newElement.innerHTML = output;
+    
+    // stampa in console
     console.log(output);
+
+    // aggiunta elemento nel DOM tramite l'utilizzo di 'append'
+    container.append(newElement);
+
 }
